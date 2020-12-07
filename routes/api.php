07b9e4754aa20c7d67246c552e3180126998edbc
afcarioca <afcarioca@hotmail.com>
2020-12-07 17:58:04 +0000

    Add EndPoints

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,6 @@ use App\Http\Controllers\NoticiaController;
 
 Route::get('noticias', [NoticiaController::class, 'index']);
 Route::get('noticias/{id}',[NoticiaController::class, 'show']);
-Route::post('noticias', [NoticiaController::class, 'store']);
-Route::put('noticias/{id}',[NoticiaController::class, 'update']);
-Route::delete('noticias/{id}',[NoticiaController::class, 'delete']);
+Route::post('noticias', [NoticiaController::class, 'store'])->middleware('jwt.auth');
+Route::put('noticias/{id}',[NoticiaController::class, 'update'])->middleware('jwt.auth');
+Route::delete('noticias/{id}',[NoticiaController::class, 'delete'])->middleware('jwt.auth');
