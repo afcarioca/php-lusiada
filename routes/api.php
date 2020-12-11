e10df0172290346ee5af478aa40f6af4f8dc9839
afcarioca <afcarioca@hotmail.com>
2020-12-11 11:42:59 +0000

    Add

--- routes/api.php
+++ routes/api.php
@@ -12,20 +12,16 @@ use App\Http\Controllers\AuthController;
 
 
 
-
-Route::post('noticias', [NoticiaController::class, 'store']);
-Route::put('noticias/{id}',[NoticiaController::class, 'update']);
-Route::delete('noticias/{id}',[NoticiaController::class, 'delete']);
+Route::get('noticias', [NoticiaController::class, 'index']);
 Route::get('noticias/{id}',[NoticiaController::class, 'show']);
-
-
-
 Route::post('login',[UserController::class, 'authenticate']);
 
 
 Route::group(['middleware' => ['jwt.verify']], function() {
     Route::get('user', [UserController::class, 'getAuthenticatedUser']);
-    Route::get('noticias', [NoticiaController::class, 'index']);
+    Route::post('noticias', [NoticiaController::class, 'store']);
+    Route::put('noticias/{id}', [NoticiaController::class, 'update']);
+    Route::delete('noticias/{id}', [NoticiaController::class, 'delete']);
 });
 
 
